ce19c15befb2fbad00aab3315766834e609587a2
Nicholas Davidson <nicholas.davidson.20@gmail.com>
2024-01-23 20:22:41 -0600

    Updated properties for ServiceTicket

--- Models/ServiceTicket.cs
+++ Models/ServiceTicket.cs
@@ -5,7 +5,9 @@
 
         public int Id { get; set; }
 
-        public string CustomerId { get; set; }
+        public int CustomerId { get; set; }
+
+        public string EmployeeId { get; set; }
 
         public string Description { get; set; }
 
