8ebff8b94f4dea1398e0226c3f5bbf86a04b1a21
Nicholas Davidson <nicholas.davidson.20@gmail.com>
2024-01-23 20:18:12 -0600

    Added properties to the data models.

---
+++ Models/Employee.cs
@@ -0,0 +1,13 @@
+﻿namespace HoneyRaesAPI.Models
+{
+    public class Employee
+    {
+
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Specialty { get; set; }
+
+    }
+}
---
+++ Models/ServiceTicket.cs
@@ -0,0 +1,16 @@
+﻿namespace HoneyRaesAPI.Models
+{
+    public class ServiceTicket
+    {
+
+        public int Id { get; set; }
+
+        public string CustomerId { get; set; }
+
+        public string Description { get; set; }
+
+        public bool Emergency { get; set; }
+
+        public DateTime DateCompleted { get; set; }
+    }
+}
